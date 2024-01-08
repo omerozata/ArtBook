70110c25df1c577c6beba5255275332cc917a0f9
Ömer Özata <omerozata015@gmail.com>
2024-01-09 01:40:13 +0300

    onBackPressed added

--- app/src/main/java/com/kuantum/artbook/view/ArtDetailsFragment.kt
+++ app/src/main/java/com/kuantum/artbook/view/ArtDetailsFragment.kt
@@ -2,7 +2,9 @@ package com.kuantum.artbook.view
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.kuantum.artbook.R
 import com.kuantum.artbook.databinding.FragmentArtDetailsBinding
 
@@ -14,7 +16,21 @@ class ArtDetailsFragment : Fragment(R.layout.fragment_art_details) {
 
         val binding = FragmentArtDetailsBinding.bind(view)
         fragmentBinding = binding
+
+        binding.imageview.setOnClickListener {
+            findNavController().navigate(ArtDetailsFragmentDirections.actionArtDetailsFragmentToImageApiFragment())
+        }
+
+        val callBack = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().popBackStack()
+            }
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(callBack)
     }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/kuantum/artbook/view/ImageApiFragment.kt
+++ app/src/main/java/com/kuantum/artbook/view/ImageApiFragment.kt
@@ -2,7 +2,9 @@ package com.kuantum.artbook.view
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.kuantum.artbook.R
 import com.kuantum.artbook.databinding.FragmentImageApiBinding
 
@@ -15,6 +17,14 @@ class ImageApiFragment : Fragment(R.layout.fragment_image_api) {
 
         val binding = FragmentImageApiBinding.bind(view)
         fragmentBinding = binding
+
+        val callBack = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().popBackStack()
+            }
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(callBack)
     }
 
     override fun onDestroyView() {
